861ede1d0e55c16adc74dd46abaddea11d36d46b
ChemseddineBouzidy <chamseddinexd@gmail.com>
2025-04-04 01:02:15 +0000

    login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
@@ -53,15 +54,27 @@ class AuthController extends Controller
                 "message" => "User not found"
             ], 401);
         }
-        if ($user->password != $request->input('passwoord')) {
+        if (!Hash::check($request->input('password'), $user->password)) {
+            // If the password does not match, return an error response
             return response()->json([
                 "message" => "Password not matched"
             ], 401);
         }
+        // If the password matches, delete any existing tokens for the user
+        // This is important to prevent multiple active tokens for the same user
+        // You can also choose to revoke all tokens instead of deleting them
         $user->tokens()->delete();
         // Create a new token for the user
         $token = $user->createToken('auth_token')->plainTextToken;
-
+ 
+         
+        // Return a success response with the token
+        return response()->json([
+            'status' => true,
+            'message' => 'User logged in successfully',
+            'user' => $user,
+            'token' => $token,
+        ], 200);
 
     }
 }
